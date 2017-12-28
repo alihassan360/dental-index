9a4d95cdf5bb85b308fb96724e185af3c53441e2
 Mohamed Azazy <azazy@symbyo.com>
2017-12-28 19:16:17 +0200

    First commit.

---
+++ gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    title: `A sample site using gatsby-source-wordpress`,
+    subtitle: `Data fetched from a site hosted on wordpress.com`,
+  },
+  plugins: [
+    // https://public-api.wordpress.com/wp/v2/sites/gatsbyjsexamplewordpress.wordpress.com/pages/
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from Wordpress.
+     */
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: `casemastersblog.wordpress.com`,
+        // The protocol. This can be http or https.
+        protocol: `https`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the asumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: true,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com
+        useACF: false,
+        auth: {
+          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
+          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
+          // then add your clientId, clientSecret, username, and password here
+          wpcom_app_clientSecret: "GZ3OazjTFMqsua76zZslJJIvrcxZtsNXiYiOTPEkqJO0BDplAwO4SLocw8tOsBpl",
+          wpcom_app_clientId: "56552",
+          wpcom_user: "azazy@symbyo.com",
+          wpcom_pass: "gP3&Jp&XK65Nc3MmHK",
+        },
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
+}
